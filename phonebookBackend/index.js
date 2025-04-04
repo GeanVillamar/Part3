d960ed762d7de13706cbbbd3e942cc7112c42a8c
Gean <geanfranklinvillamar@gmail.com>
2025-04-04 11:10:11 -0500

    3.3: phonebook backend, step 3

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -39,6 +39,17 @@ app.get("/info", (request, response) => {
   );
 });
 
+//step 3: get a person by id, http://localhost:3001/api/persons/5
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
